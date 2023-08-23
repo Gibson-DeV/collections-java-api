5983abda79b08b1c873c7c24e6f1df7dabe874e7
Gibson Cruz <gibson.cruz@fesudeperj.com>
2023-08-23 13:11:36 -0300

    Resovlendo Desafio ListaTarefa

---
+++ src/list/OperacoesBasicas/Main.java
@@ -0,0 +1,23 @@
+package list.OperacoesBasicas;
+ public class Main{
+     public static void main(String[] args) {
+
+         ListaTarefa lista1 = new ListaTarefa();
+
+        lista1.adicionarTarefa("Tarefa 01");
+        lista1.adicionarTarefa("Tarefa 02");
+        lista1.adicionarTarefa("Tarefa 03");
+        lista1.adicionarTarefa("Tarefa 04");
+
+        lista1.removerTarefa("Tarefa 03");
+
+        System.out.println(lista1.obterNumeroTotalTarefas());
+
+        lista1.obterDescricaoTarefas();
+
+
+
+
+
+     }
+ }
